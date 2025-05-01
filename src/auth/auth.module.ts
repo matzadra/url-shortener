6f5ce548e679c2ca42505dfc8e06010924a35782
matzadra <matheuszadra@gmail.com>
2025-05-01 01:31:19 -0300

    refactor(auth): injeta ConfigService no JwtModule

---
+++ src/auth/auth.module.ts
@@ -0,0 +1,26 @@
+import { Module } from "@nestjs/common";
+import { ConfigService } from "@nestjs/config";
+import { AuthService } from "./auth.service";
+import { AuthController } from "./auth.controller";
+import { JwtModule } from "@nestjs/jwt";
+import { PassportModule } from "@nestjs/passport";
+import { JwtStrategy } from "./strategies/jwt.strategy";
+import { PrismaService } from "../prisma/prisma.service";
+import { UsersModule } from "../users/users.module";
+
+@Module({
+  imports: [
+    PassportModule,
+    JwtModule.registerAsync({
+      inject: [ConfigService],
+      useFactory: (config: ConfigService) => ({
+        secret: config.get<string>("JWT_SECRET"),
+        signOptions: { expiresIn: "1d" },
+      }),
+    }),
+    UsersModule,
+  ],
+  controllers: [AuthController],
+  providers: [AuthService, JwtStrategy, PrismaService],
+})
+export class AuthModule {}
