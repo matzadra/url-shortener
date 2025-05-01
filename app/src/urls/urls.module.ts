2207a7b53e8dbb3ee366f8fcd1641e0519462b22
matzadra <matheuszadra@gmail.com>
2025-05-01 01:09:22 -0300

    chore: remove estrutura duplicada da pasta /app

--- app/src/urls/urls.module.ts
+++
@@ -1,9 +0,0 @@
-import { Module } from '@nestjs/common';
-import { UrlsController } from './urls.controller';
-import { UrlsService } from './urls.service';
-
-@Module({
-  controllers: [UrlsController],
-  providers: [UrlsService]
-})
-export class UrlsModule {}
