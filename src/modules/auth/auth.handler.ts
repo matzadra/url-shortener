e103c23ea8c235c3717429c58aa44346a64f52c2
matzadra <matheuszadra@gmail.com>
2025-05-26 14:31:27 -0300

    refactor

--- src/modules/auth/auth.handler.ts
+++ src/modules/auth/auth.handler.ts
@@ -1,14 +1,18 @@
 import { Body, Controller, Post, HttpCode } from "@nestjs/common";
 import { AuthService } from "@modules/auth/auth.service";
 import { LoginDto } from "@modules/auth/dto/login.dto";
+import { AuthResponseDto } from "./dto/auth-response.dto";
+import { ApiTags, ApiResponse } from "@nestjs/swagger";
 
+@ApiTags("Auth")
 @Controller("auth")
 export class AuthHandler {
   constructor(private readonly authService: AuthService) {}
 
   @HttpCode(200)
   @Post("login")
-  async login(@Body() dto: LoginDto) {
+  @ApiResponse({ status: 200, type: AuthResponseDto })
+  async login(@Body() dto: LoginDto): Promise<AuthResponseDto> {
     return this.authService.login(dto);
   }
 }
